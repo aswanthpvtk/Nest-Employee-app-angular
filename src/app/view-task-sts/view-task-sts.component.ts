e04b49e21e780c19c7185f168c6b18f3dfcc76df
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-11 23:24:44 +0530

    nest

---
+++ src/app/view-task-sts/view-task-sts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-task-sts',
+  templateUrl: './view-task-sts.component.html',
+  styleUrls: ['./view-task-sts.component.css']
+})
+export class ViewTaskStsComponent {
+
+}
